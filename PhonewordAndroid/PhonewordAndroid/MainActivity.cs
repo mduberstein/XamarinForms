b93e63cf50cf2e68d2f336b5c6f7a5de92e80388
Michael Duberstein <michaeld@Michaels-MacBook-Pro.local>
2018-05-20 19:50:51 -0400

    First Actual commit into XamarinForms repo with Phoneword as submodule

---
+++ PhonewordAndroid/PhonewordAndroid/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PhonewordAndroid
+{
+    [Activity(Label = "Phone Word Android", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our UI controls from the loaded layout
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneWord);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+        
+            // Add code to translate number
+            string translatedNumber = string.Empty;
+
+            translateButton.Click += (sender, e) =>
+            {
+                // Translate user's alphanumeric phone number to numeric
+                translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = translatedNumber;
+                }
+            };
+        }
+    }
+}
+
